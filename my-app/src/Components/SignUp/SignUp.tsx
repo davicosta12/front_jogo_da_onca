3a0484327321d0c805759f547439962ffee38ed4
Davi <davi.costa@ajdesenvolvimento.com.br>
2022-10-29 19:24:53 -0300

    ajustes estilos

--- my-app/src/Components/SignUp/SignUp.tsx
+++ my-app/src/Components/SignUp/SignUp.tsx
@@ -29,49 +29,54 @@ const SignUp: FunctionComponent<Props> = (props) => {
 
   return (
     <Segment className='signUp-container'>
+      
       <div className='signUp-header'>
         <h1 className='login-title'>Criar conta</h1>
       </div>
       <div className='signUp-content'>
+
         <div className='mb-3'>
           <h3 className='login-subtitle text-center'>Forneça suas informações</h3>
         </div>
-        <Form>
-          <Form.Field>
-            <Input
-              placeholder='Nome'
-              size='large'
-              value={userName}
-              onChange={e => setUserName(e.target.value)}
-            />
-          </Form.Field>
 
-          <Form.Field>
-            <Input
-              placeholder="Email"
-              size='large'
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-          </Form.Field>
+        <div className='signUp-form-container'>
+          <Form>
+            <Form.Field>
+              <Input
+                placeholder='Nome'
+                size='large'
+                value={userName}
+                onChange={e => setUserName(e.target.value)}
+              />
+            </Form.Field>
 
-          <Form.Field>
-            <Input
-              placeholder="Senha"
-              size='large'
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
-          </Form.Field>
+            <Form.Field>
+              <Input
+                placeholder="Email"
+                size='large'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+            </Form.Field>
 
-          <Form.Field>
-            <Input
-              placeholder="Confirmar senha"
-              size='large'
-              value={confirmPassword}
-              onChange={e => setConfirmPassword(e.target.value)}
-            />
-          </Form.Field>
+            <Form.Field>
+              <Input
+                placeholder="Senha"
+                size='large'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </Form.Field>
+
+            <Form.Field>
+              <Input
+                placeholder="Confirmar senha"
+                size='large'
+                value={confirmPassword}
+                onChange={e => setConfirmPassword(e.target.value)}
+              />
+            </Form.Field>
+          </Form>
 
           <div className='flex flex-wrap justify-content-end mt-2'>
             <div className='flex justify-content-center align-items-center'>
@@ -81,10 +86,10 @@ const SignUp: FunctionComponent<Props> = (props) => {
               <Button className='p-button-signInAndSignUp' onClick={handleSignUp}>Cadastrar</Button>
             </div>
           </div>
+        </div>
 
-        </Form>
       </div>
-    </Segment>
+    </Segment >
   );
 };
 
