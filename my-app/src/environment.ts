7efb5744fac6183efc8839f2b7456798ffe8f553
Davi Costa <davi.costa@ajdesenvolvimento.com.br>
2022-11-29 11:03:42 -0300

    ajuste baseUrl

--- my-app/src/environment.ts
+++ my-app/src/environment.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://jogodaonca.azurewebsites.net/api";
+export const API_BASE_URL = "jogodaonca.azurewebsites.net/api";
 export const TOKEN_KEY = "@airbnb-Token";
 export const ACTIVE_USER = "ACTIVE_USER";
