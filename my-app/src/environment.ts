99eb75ac1fb3ef0621c497c61804d10f6ae9d49e
Davi Costa <davi.costa@ajdesenvolvimento.com.br>
2022-11-29 11:31:24 -0300

    retirando heroku

--- my-app/src/environment.ts
+++ my-app/src/environment.ts
@@ -1,3 +1,4 @@
-export const API_BASE_URL = "https://jogodaonca.azurewebsites.net/api";
+//export const API_BASE_URL = "https://jogodaonca.azurewebsites.net/api";
+export const API_BASE_URL = "https://localhost:7221/api";
 export const TOKEN_KEY = "@airbnb-Token";
 export const ACTIVE_USER = "ACTIVE_USER";
