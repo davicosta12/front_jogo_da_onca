f76eeae396840037ca3d1b595a2d6b68fba1d9cb
Davi <davi.costa@ajdesenvolvimento.com.br>
2022-11-29 00:57:17 -0300

    retirando azure

--- my-app/src/environment.ts
+++ my-app/src/environment.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://jogodaonca.azurewebsites.net/api";
+export const API_BASE_URL = "localhost:7221/api";
 export const TOKEN_KEY = "@airbnb-Token";
 export const ACTIVE_USER = "ACTIVE_USER";
